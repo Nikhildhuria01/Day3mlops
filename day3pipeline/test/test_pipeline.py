161f8f1177ea4dc24ae47aa17d123cd5f914058e
Nikhildhuria01 <nikhilgg008@gmail.com>
2026-06-05 09:46:19 +0530

    changing name22

--- day3pipeline/test/test_pipeline.py
+++ day3pipeline/test/test_pipeline.py
@@ -1,4 +1,8 @@
-from day3pipeline.data_loader import load_data
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from data_loader import load_data
 
 
 def test_load_data():
